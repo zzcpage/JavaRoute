1ce4966e771acf4bd004ed22ad62a59ec549cc51
zzc <2891112980@qq.com>
2021-10-25 15:28:54 +0800

    第一次测试

---
+++ git.md
@@ -0,0 +1,4 @@
+[1. IDEA对项目进行Git操作](#one)
+### IDEA对项目进行Git操作<div id="one"></div>
+    1. 可以通过git,在项目文件夹下创建一个init文件夹，然后就可以通过IDEA对项目进行管理。
+    2. 然后可以通过IDEA顶部的VCS下的git,设定git的远程地址，以及对项目进行相关操作.
---
+++ java_parent/src/main/java/main.java
@@ -0,0 +1,12 @@
+
+/**
+ * hello , 欢迎来到java的世界，开始学习java了！！！
+ * 冲冲冲！！！
+ * @author : zzc
+ * @date: 2021-10-20 14:35
+ **/
+public class main {
+    public static void main(String args[]) {
+        System.out.println("hello world");
+    }
+}
